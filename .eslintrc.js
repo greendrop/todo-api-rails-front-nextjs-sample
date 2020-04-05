ae69ea177af0568ea812f4120f13131e7fbdcc3c
greendrop <greendrop.devel@gmail.com>
2020-04-05 10:18:33 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,108 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+    'prettier',
+    'prefer-arrow',
+    'react',
+    'react-hooks'
+  ],
+  root: true,
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', 'jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  rules: {
+    // eslint official
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'require-yield': 'error',
+
+    // @typescript-eslint
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/interface-name-prefix': [
+      'error',
+      {
+        'prefixWithI': 'always'
+      }
+    ],
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false
+        }
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+
+    // prefer-arrow
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: true,
+        classPropertiesAllowed: false
+      }
+    ],
+
+    // react
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['jsx', 'tsx']
+      }
+    ],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/prop-types': 'off',
+    'react/prefer-stateless-function': 'off',
+
+    // react hooks
+    'react-hooks/rules-of-hooks': 'error',
+
+    // prettier
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        printWidth: 80,
+        semi: false,
+        singleQuote: true,
+        useTabs: false
+      }
+    ]
+  }
+}
